054aa7fff46de5a9c52a1e5677547e502d3a9fc7
Brandon Rhodes <brandon@rhodesmill.org>
2013-07-20 01:27:28 -0400

    Example data that might go in personal.py

--- orginvoicer/personal.py
+++ orginvoicer/personal.py
@@ -1,4 +1,23 @@
+from datetime import datetime
 from decimal import Decimal
 
+raise_date = datetime(2012, 6, 20)
+
 def compute_rate(date):
-    return Decimal('60.')
+    return Decimal('60' if date < raise_date else '75')
+
+address_paragraph = """
+<i>From:</i><br/>
+<br/>
+<b>Example, Inc.</b><br/>
+Main Street<br/>
+solutions@example.com<br/>
+<br/>
+<i>Bill to:</i><br/>
+<br/>
+<b>Happy Customer, LLC</b><br/>
+http://happy-customer.com/<br/>
+<br/>
+<i>Date:</i><br/><br/>July 20, 2013<br/><br/>
+<i>Terms:</i><br/><br/>30 days<br/><br/>
+"""
